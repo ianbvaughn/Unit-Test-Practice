e98029c5297c73fa2cb5ee0da360d5fdb46b313c
Ian Vaughn <ianbvaughn@gmail.com>
2024-11-24 19:52:46 -0800

    Modifications

--- Cart.py
+++ Cart.py
@@ -1,9 +1,12 @@
 class ShoppingCart:
-    def __init__(self):
+    def __init__(self,cart_limit):
         self._items=[]
+        self._cart_limit = cart_limit
 
     def add_item(self,item):
+        if self.get_num_items() == self._cart_limit:
+            raise OverflowError("Cannot add additional items!")
         self._items.append(item)
 
-    def get_items(self):
-        return self._items
+    def get_num_items(self):
+        return len(self._items)
--- test_cart.py
+++ test_cart.py
@@ -3,9 +3,16 @@ import pytest
 from Cart import ShoppingCart
 
 def test_create_cart():
-    assert ShoppingCart()
+    assert ShoppingCart(1)
 
 def test_add_items():
-    test_cart = ShoppingCart()
+    test_cart = ShoppingCart(1)
     test_cart.add_item("lettuce")
-    assert len(test_cart.get_items()) != 0
+    assert test_cart.get_num_items() != 0
+
+def test_exceed_cart_limit():
+    test_cart = ShoppingCart(5)
+    with pytest.raises(OverflowError):
+        for i in range(6):
+            test_cart.add_item(i)
+        pass
